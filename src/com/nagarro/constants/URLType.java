14e26098910e0373bca053129152cf3d7bed2b4e
Shivam Adolia <shivam.adolia@nagarro.com>
2021-05-21 14:16:23 +0530

    Initial commit

--- src/com/nagarro/constants/URLType.java
+++ src/com/nagarro/constants/URLType.java
@@ -3,10 +3,10 @@ package com.nagarro.constants;
 public class URLType {
 
 	
-	public static final String GET_EMPS_URL = "http://localhost:8080/employees/webapi/employees";
-	public static final String GET_EMP_URL = "http://localhost:8080/employees/webapi/employees/employee/";
-	public static final String UPLD_EMP_URL = "http://localhost:8080/employees/webapi/employees/employee";
-	public static final String UPDT_EMP_URL = "http://localhost:8080/employees/webapi/employees/employee";
-	public static final String DLT_EMP_URL = "http://localhost:8080/employees/webapi/employees/employee/";
+	public static final String GET_EMPS_URL = "http://localhost:8083/employees/webapi/employees";
+	public static final String GET_EMP_URL = "http://localhost:8083/employees/webapi/employees/employee/";
+	public static final String UPLD_EMP_URL = "http://localhost:8083/employees/webapi/employees/employee";
+	public static final String UPDT_EMP_URL = "http://localhost:8083/employees/webapi/employees/employee";
+	public static final String DLT_EMP_URL = "http://localhost:8083/employees/webapi/employees/employee/";
 
 }
